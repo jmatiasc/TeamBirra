799db364076cb9fcf18349f2f7a13226f1f1ff72
Federico <federicomargueliche@gmail.com>
2019-11-12 23:35:23 -0300

    Template basico de la pagina web

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="mastfoot mt-auto text-center">
+	<div class="inner">
+		<p><a href="./index.php">TEAM BIRRA</a>. Todos los derechos reservados.</p>
+	</div>
+</footer>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<?=!include_once('head.php'); ?>
+
+<body>
+	<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+		
+		<?=!include_once('header.php'); ?>
+
+		<main role="main" class="inner cover text-center">
+			<h1 class="cover-heading">Birra, birra y mas birra.</h1>
+			<p class="lead">Sarasa como un lorem ipsum o lo que se te pueda ocurrir. Sarasa como un lorem ipsum o lo que se te pueda ocurrir. Sarasa como un lorem ipsum o lo que se te pueda ocurrir. Sarasa como un lorem ipsum o lo que se te pueda ocurrir. Sarasa como un lorem ipsum o lo que se te pueda ocurrir.</p>
+			<p class="lead">
+				<a href="#" class="btn btn-lg btn-secondary">Leer mas</a>
+			</p>
+		</main>
+
+		<?=!include_once('footer.php'); ?>
+
+	</div>
+
+	<?=!include_once('scripts.php'); ?>	
+
+</body>
+</html>

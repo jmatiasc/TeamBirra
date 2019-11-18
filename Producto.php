903af924d873a38c470b3e998dbf22ec853b2a83
unknown <jmatias_c@outlook.com>
2019-11-18 16:47:11 -0300

    correccion catalgo, producto y style. agregacion de imagenes

--- Producto.php
+++ Producto.php
@@ -2,9 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<title>Producto</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/styles3.css">
+		<link rel="stylesheet" href="css/styles2.css">
     <link href="https://fonts.googleapis.com/css?family=Catamaran&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
     <?=!include_once('head.php'); ?>
@@ -24,32 +25,33 @@
 <section class="cuerpo">
 
   <section class="imagen">
-    	<img class="photo" src="images/img-pdto-1.jpg" alt="pdto 01">
+    	<img class="photo" src="images/quilmes.png" alt="pdto 01">
 
   </section>
 
 
 		<!-- productos -->
 		<section class="datos">
-	       <h2>Nombre del Producto</h2>
+	       <h2>Qulimes</h2>
 		     <h6>linea Quilmes</h6>
-         <h3>$100000</h3>
-         <p>_______________________________________________</p>
+         <h3>$55</h3>
+         <p>____________________________________________________________________</p>
          <h3>Descripción:</h3>
-         <p>este producto te la da en la pero, pero mal <br>
-            sdasd asd gfd ew tre cvsd sdfsdfs <br> </p>
-        <p>Graduacion alcoholica: xx</p>
-		</section>
+         <p>Es una cerveza equilibrada, de gran refrescancia y cuerpo balanceado,<br>
+					  que marida con platos más bien grasosos o pesados. La gasificación de <br>
+						esta cerveza barre el paladar manteniendo las notas de sabores en la boca,<br>
+						 y su gran refrescancia invita a comer acompañando siempre con la bebida.<br>
+						 Recomendamos acompañarla con tostado clásico, milanesa con papas fritas, <br>
+						 pizza de jamón y morrones.
+ <br> </p>
+        <p><strong>Graduacion alcoholica:</strong> xx </p>
+				<p><strong>Origen: </strong> Argentina</p>
+
+				<section class="btn-comprar">
+			      <a href="#" class="boton-Comprar btn btn-lg btn-secondary">COMPRAR</a>
+			      </section>
+			  </section>
     <p>______________________________________________________________________________________________________________________________________________</p>
-
-    <section class="btn-comprar">
-      <a href="#" class="boton-Comprar btn btn-lg btn-secondary">COMPRAR</a>
-    </section>
-
-
-
-
-
 
 </section>
 <br><br><br><br>
--- catalogo.php
+++ catalogo.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+			<title>Catalogo</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/styles2.css">
@@ -27,57 +28,61 @@
       <ul class="menu">
         <li> <a href="#">Marcas</a>
           <ul>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
+            <li> <a href="#">Andes Origen</a> </li>
+            <li> <a href="#">Brahma</a> </li>
+            <li> <a href="#">Budweiser</a> </li>
+            <li> <a href="#">Estrella Galicia</a> </li>
+            <li> <a href="#">Hoegaarden</a> </li>
+            <li> <a href="#">Peñon del Aguila</a> </li>
+						<li> <a href="#">Stella Artois</a> </li>
+						<li> <a href="#">Crimen Beer</a> </li>
           </ul>
         </li>
 
         <li> <a href="#">Origen</a>
           <ul>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
+						<li> <a href="#">Alemania</a> </li>
+            <li> <a href="#">Argentina</a> </li>
+            <li> <a href="#">Belgica</a> </li>
+            <li> <a href="#">España</a> </li>
+            <li> <a href="#">Holanda</a> </li>
+            <li> <a href="#">Italia</a> </li>
+            <li> <a href="#">Perú</a> </li>
           </ul>
         </li>
 
 
         <li> <a href="#">Estilos</a>
           <ul>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
+            <li> <a href="#">Golden Ale</a> </li>
+            <li> <a href="#">Red Lager</a> </li>
+            <li> <a href="#">Pale Lager</a> </li>
+            <li> <a href="#">Scotch Ale</a> </li>
+            <li> <a href="#">Amber Lager</a> </li>
+            <li> <a href="#">American Ipa</a> </li>
+						<li> <a href="#">Pilsen</a> </li>
           </ul>
         </li>
 
         <li> <a href="#">color</a>
           <ul>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
-            <li> <a href="#">Quilmes</a> </li>
+            <li> <a href="#">Ambar</a> </li>
+            <li> <a href="#">Dorado</a> </li>
+            <li> <a href="#">Negra</a> </li>
+            <li> <a href="#">Oscuro</a> </li>
+            <li> <a href="#">Rubí</a> </li>
+            <li> <a href="#">Roja</a> </li>
           </ul>
         </li>
 
-        <li> <a href="#">Graduacion Alcoholica</a>
+        <li > <a href="#">Graduacion Alcoholica</a>
           <ul>
-            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="7">7%</a> </li>
-            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="10">10%</a> </li>
-            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="15">15%</a> </li>
-            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="20">20%</a> </li>
-            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="30">30%</a> </li>
-            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="70">70%</a> </li>
+            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="2">2%</a> </li>
+            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="25">2.5%</a> </li>
+            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="45">4.5%</a> </li>
+            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="6">6%</a> </li>
+            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="73">7.3%</a> </li>
+            <li> <a href="#"><input type="checkbox" name="graduacionAlcoholica" value="12">12%</a> </li>
           </ul>
         </li>
 
@@ -85,111 +90,245 @@
     </nav>
 
 		<!-- productos -->
-		<section class="vip-products">
-			<article class="product">
-				<div class="photo-container">
-					<img class="photo"  src="images/img-pdto-1.jpg" alt="pdto 01">
-					<img class="special" src="images/img-nuevo.png" alt="plato nuevo">
-          <h3>$100000</h3>
-			</div>
-				<a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="boton-Comprar btn btn-lg btn-secondary">COMPRAR</a>
 
-			</article>
-			<article class="product">
-				<div class="photo-container">
-					<img class="photo" src="images/img-pdto-2.jpg" alt="pdto 02">
-					<img class="special" src="images/img-gratis.png" alt="gratis más de 3">
-          <h3>$100000</h3>
-				</div>
-				<a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-			</article>
-			<article class="product">
-				<div class="photo-container">
-					<img class="photo" src="images/img-pdto-3.jpg" alt="pdto 03">
-					<img class="special" src="images/img-descuento.png" alt="descuento efectivo">
-          <h3>$100000</h3>
-				</div>
-				<a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-			</article>
-			<article class="product">
-				<div class="photo-container">
-					<img class="photo" src="images/img-pdto-1.jpg" alt="pdto 01">
-					<img class="special" src="images/img-gratis.png" alt="gratis más de 3">
-          <h3>$100000</h3>
-				</div>
-				<a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-			</article>
-			<article class="product">
-				<div class="photo-container">
-					<img class="photo" src="images/img-pdto-2.jpg" alt="pdto 02">
-					<img class="special" src="images/img-nuevo.png" alt="plato nuevo">
-          <h3>$100000</h3>
-				</div>
-				<a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-			</article>
-			<article class="product">
-				<div class="photo-container">
-					<img class="photo" src="images/img-pdto-3.jpg" alt="pdto 03">
-					<img class="special" src="images/img-descuento.png" alt="descuento efectivo">
-          <h3>$100000</h3>
-				</div>
-				<a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-			</article>
-      <article class="product">
-        <div class="photo-container">
-          <img class="photo" src="images/img-pdto-2.jpg" alt="pdto 02">
-          <img class="special" src="images/img-nuevo.png" alt="plato nuevo">
-          <h3>$100000</h3>
-        </div>
-        <a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-      </article>
-      <article class="product">
-        <div class="photo-container">
-          <img class="photo" src="images/img-pdto-3.jpg" alt="pdto 03">
-          <img class="special" src="images/img-descuento.png" alt="descuento efectivo">
-          <h3>$100000</h3>
-        </div>
-        <a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-      </article>
-      <article class="product">
-        <div class="photo-container">
-          <img class="photo" src="images/img-pdto-2.jpg" alt="pdto 02">
-          <img class="special" src="images/img-nuevo.png" alt="plato nuevo">
-          <h3>$100000</h3>
-        </div>
-        <a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-      </article>
-      <article class="product">
-        <div class="photo-container">
-          <img class="photo" src="images/img-pdto-3.jpg" alt="pdto 03">
-          <img class="special" src="images/img-descuento.png" alt="descuento efectivo">
-          <h3>$100000</h3>
-        </div>
-        <a class="more" href="Producto.php"><h5>Lorem ipsum amet</h5></a>
-        <p>descripcion</p>
-        <a href="#" class="btn btn-lg btn-secondary">COMPRAR</a>
-      </article>
-      <section class="cambiarPagArticulos">
-        (<)   (1)    (2)    (3)  ..  (70)    (>)
-      </section>
+
+		<?php $productos = [
+
+	0 => [
+		"nombre" =>"Andes Ipa",
+
+		"imagen" => "images/andes.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 85,
+
+		"descripcion" =>"descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Argentina"
+
+	],
+	1 => [
+			"nombre" =>"Andes Rubia",
+
+		"imagen" => "images/andesR.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 80,
+
+		"descripcion" => "descripcion Lorem ipsum dolor sit amet ",
+
+    "color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Argentina"
+
+	],
+	2 => [
+			"nombre" =>"Brahma",
+
+		"imagen" => "images/brahma.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 45,
+
+		"descripcion" => "descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Brasil"
+	],
+	3 => [
+			"nombre" =>"Budweiser",
+
+		"imagen" => "images/bud.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 60,
+
+		"descripcion" => "descripcion Lorem ipsum dolor sit amet ",
+
+	  "color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Argentina"
+
+	],
+	4 => [
+			"nombre" =>"Peñon",
+
+		"imagen" => "images/pen-lar.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 130,
+
+		"descripcion" =>"descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Argentina"
+
+	],
+	5 => [
+			"nombre" =>"Peñon Dark",
+
+		"imagen" => "images/peñon1.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 180,
+
+		"descripcion" =>"descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Negro",
+
+		"Estilo" => "Scotch",
+
+		"Origen" => "Argentina"
+
+	],
+	6 => [
+			"nombre" =>"Peñon Cacao-Ipa",
+
+		"imagen" => "images/peñon3.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 130,
+
+		"descripcion" =>"descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "IPA",
+
+		"Origen" => "Argentina"
+
+	],
+	7 => [
+			"nombre" =>"Stella Artois",
+
+		"imagen" => "images/Stella.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 65,
+
+		"descripcion" =>"descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Belgica"
+
+	],
+	8 => [
+			"nombre" =>"Hoegaarden",
+
+		"imagen" => "images/hoagg.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 300,
+
+		"descripcion" => "descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Alemania"
+
+	],
+	9 => [
+		"nombre" =>"Peñon Arandano",
+
+		"imagen" => "images/peñon2.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 100,
+
+		"descripcion" => "descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Morado",
+
+		"Estilo" => "Pale Lager",
+
+		"Origen" => "Argentina"
+
+	],
+	10 => [
+			"nombre" =>"Quilmes",
+
+		"imagen" => "images/quilmes.png",
+
+		"enOferta" => true,
+
+		"esNovedad" => true,
+
+		"precio" => 55,
+
+		"descripcion" => "descripcion Lorem ipsum dolor sit amet ",
+
+		"color " => "Dorado",
+
+		"Estilo" => "Blonde",
+
+		"Origen" => "Argentina"
+
+	]
+]; ?>
+		<section class="catalogo-productos">
+			<?php foreach ($productos as $producto) : ?>
+									<article class="product">
+										<div class="contenedor-foto">
+											<img class="foto"  src="<?php echo $producto["imagen"] ?>" alt="pdto 01">
+
+						          <h3>$ <?php echo  $producto["precio"] ?> </h3>
+									</div>
+										<a class="nombre" href="Producto.php"><h5><?php echo  $producto["nombre"] ?></h5></a>
+						        <p><?php echo  $producto["descripcion"] ?></p>
+						        <a href="#" class="boton-Comprar btn btn-lg btn-secondary">COMPRAR</a>
+
+
+						      </article>
+			<?php endforeach ?>
 
 		</section>
 

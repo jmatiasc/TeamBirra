e4bc92c95f42468ebe9ad16a114314303c15d504
unknown <jmatias_c@outlook.com>
2019-12-06 13:05:20 -0300

    modificacion en login y resgister+persistencia

--- login.php
+++ login.php
@@ -1,3 +1,63 @@
+<?php
+$error=[];
+if($_POST){
+
+
+  /* se abre el archivo y se lo guarda en la variable llamada $contenidoArchico (todavia no tiene ningun formato definido)*/
+  $contenidoArchivo=file_get_contents("archivos/user.txt");
+
+  /*se le da formato al contenido del archivo guardandolo en la variable llamada $arratyUsuarios*/
+  $arrayUsuarios=json_decode($contenidoArchivo, true);
+
+  if(isset($_POST["email"]) && strlen($_POST["email"])!=0){
+          $usuario["email"]=$_POST["email"];
+  }else {
+    $error[1]="no ingreso email";
+  }
+
+
+       if(isset($_POST["password"]) && strlen($_POST["password"])!=0){
+             $usuario["password"]=password_hash($_POST["password"],PASSWORD_DEFAULT);
+     }else {
+       $error[2]="no ingreso password";
+     }
+
+
+  if(count($error)==0){
+					      /*solo para mostrar el archivo
+					      var_dump($arrayUsuarios);*/
+
+
+					      $cont=0;
+					      /*se buscará en el archivo que exista un usuario logueado con el email ingresado
+					        y la contraseña correspondiente  */
+					      foreach ($arrayUsuarios as $key => $value) {
+					        if(($value[$cont]["email"]==$_POST["email"]) ){
+									 	if(password_verify($_POST["password"],$value[$cont]["password"]))
+					              {
+													/*SOLO PARA PROVAR QUE FUNCIONA, APARECE ARRIBA A LA IZQUIERDA SI EL USUARIO EXISTE*/
+					                  echo "BIENVENIDO:";
+					                  echo $value[$cont]["name"];
+
+					                  /*ACA DEBERIA REDIRIGIR LA PAGINA*/
+														header("Location:index.php");
+														exit;
+					              }else {echo "contraseña incorrecta";
+
+																}
+					          }
+										$cont++;
+					      }
+
+								if($cont==count($arrayUsuarios)) echo "no se encontro usuario";
+
+			 }else var_dump($error);
+
+}/*fin del if(!$_POST)*/
+ ?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,32 +66,46 @@
 </head>
 <body>
 	<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-		
+
 		<?=!include_once('header.php'); ?>
 
-		<main role="main" class="inner cover text-center">			
+		<main role="main" class="inner cover text-center">
 			<section class="login-block">
 				<div class="col-md-8 offset-md-2 col-sm-8 offset-sm-2">
 					<div class="container-login">
 						<div class="row ">
 							<div class="col login-sec">
 								<h3 class="text-center">Iniciar Sesion</h3>
-								<form class="login-form">
-									<div class="form-group">
-										<label for="exampleInputEmail1">Email</label>
-										<input type="email" name="email" id="email" class="form-control" placeholder="" required>
-									</div>
-									<div class="form-group">
-										<label for="exampleInputPassword1">Contraseña</label>
-										<input type="password" name="password" id="password" class="form-control" placeholder="" required>
-									</div>
-									<div class="form-check">
-										<label class="form-check-label">
-											<input type="checkbox" name="remember" id="remember" class="form-check-input">
-											<small>Recordarme</small>
-										</label>
-										<button type="submit" class="btn btn-secondary float-right">Iniciar Sesion</button>
-									</div>
+								<form class="login-form" action='login.php' method='POST' >
+												   <fieldset >
+														<!-- DIV DEL INGRESO DE EMAIL-->
+														<div class="form-group">
+															<label for="exampleInputEmail1">Email</label>
+															<input type="email" name="email" id="email" class="form-control" placeholder="" required>
+															<span id='register_email_errorloc' class='error'></span>
+														</div>
+
+														<!-- DIV DEL INGRESO DE PASSWORD-->
+														<div class="form-group">
+														<label for="exampleInputPassword1">Contraseña</label>
+														<input type="password" name="password" id="password" class="form-control" placeholder="" required>
+														<div id='register_password_errorloc' class='error' style='clear:both'></div>
+													</div>
+
+													<!-- CHECKBOX DE REMEMBER-->
+													<div class="form-check">
+														<label class="form-check-label">
+														<input type="checkbox" name="remember" id="remember" class="form-check-input">
+														<small>Recordarme</small>
+														</label>
+
+												</div>
+
+												<div>
+													<button type="submit" class="btn btn-secondary float-right">Iniciar Sesion</button>
+												</div>
+
+										   </fieldset>
 								</form>
 							</div>
 						</div>
@@ -44,7 +118,7 @@
 
 	</div>
 
-	<?=!include_once('scripts.php'); ?>	
+	<?=!include_once('scripts.php'); ?>
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,3 +1,76 @@
+<?PHP
+
+$error=[];
+/*se crea un array para guardar los errores que se encontraron en la validacion y luego mostrarlos*/
+if($_POST){
+     if(isset($_POST["name"]) && strlen($_POST["name"])!=0){
+           $usuario["name"]=$_POST["name"];
+     }else {
+       $error[0]="no ingreso nombre";
+     }
+
+
+		 if(isset($_POST["apellido"]) && strlen($_POST["apellido"])!=0){
+					 $usuario["apellido"]=$_POST["apellido"];
+		 }else {
+			 $error[0]="no ingreso Apellido";
+		 }
+
+     if(isset($_POST["email"]) && strlen($_POST["email"])!=0){
+			 					if(isset($_POST["c_email"]) && strlen($_POST["c_email"])!=0 &&	($_POST["c_email"]==$_POST["email"]))
+             		{
+
+									$usuario["email"]=$_POST["email"];}
+							else {
+								echo $_POST["email"];
+								echo $_POST["c_email"];
+				        $error[1]="error en la confiarmacion de email";
+				      }
+     }else {
+       $error[1]="no ingreso email";
+     }
+
+
+     if(isset($_POST["password"]) && strlen($_POST["password"])!=0){
+			  if(isset($_POST["c_password"]) && strlen($_POST["c_password"])!=0 &&	$_POST["c_password"]==$_POST["password"])
+          { $usuario["password"]=password_hash($_POST["password"],PASSWORD_DEFAULT);}
+				else  $error[3]="las contraseñas no coinciden";
+   }else {
+     $error[3]="no ingreso password";
+   }
+
+   /*si no hay errores el usuario se guardara en nuesto archivo de registo de usuarios*/
+  if(count($error)==0){
+    $usuario = [
+      "name" => $_POST["name"],
+			"apellido" => $_POST["apellido"],
+      "email" =>$_POST["email"],
+      "password" => password_hash($_POST["password"],PASSWORD_DEFAULT),
+    ];
+
+    /* se abre el archivo y se lo guarda en la variable llamada $contenidoArchico (todavia no tiene ningun formato definido)*/
+    $contenidoArchivo=file_get_contents("archivos/user.txt");
+
+    /*se le da formato al contenido del archivo guardandolo en la variable llamada $arratyUsuarios*/
+    $arrayUsuarios=json_decode($contenidoArchivo, true);
+
+
+    /*se guarda al usuario logueado en el archivo*/
+    $arrayUsuarios["usuarios"][] = $usuario;
+
+    /*se vuelve a guardar en el archivo todos los usuarios, con el nuevo usuario recien creado*/
+    file_put_contents("archivos/user.txt", json_encode($arrayUsuarios));
+
+		/*LUEGO DE QUE SE ESCRIBIO EL ARCHIVO DE USUARIOS DEBERIA REDIRECCIONAR*/
+		header("Location:login.php");
+		exit;
+
+  }else
+
+  var_dump($error);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,18 +79,18 @@
 </head>
 <body>
 	<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-		
+
 		<?=!include_once('header.php'); ?>
 
-		<main role="main" class="inner cover text-center">			
-			<form role="form" method="post" action="#">
+		<main role="main" class="inner cover text-center">
+			<form role="form" action='register.php' method='POST' >
 				<div class="col-md-8 offset-md-2 col-sm-8 offset-sm-2">
 					<h3>Registro</h3>
 					<br>
 					<div class="row">
 						<div class="col-xs-6 col-sm-6 col-md-6">
 							<div class="form-group">
-								<input type="text" name="nombre" id="nombre" class="form-control input-sm" placeholder="Nombre" required>
+								<input type="text" name="name" id="name" class="form-control input-sm" placeholder="Nombre" required>
 							</div>
 						</div>
 						<div class="col-xs-6 col-sm-6 col-md-6">
@@ -30,11 +103,11 @@
 					<div class="form-group">
 						<input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email" required>
 					</div>
-					
+
 					<div class="form-group">
 						<input type="email" name="c_email" id="c_email" class="form-control input-sm" placeholder="Confirmar Email" required>
 					</div>
-					
+
 					<div class="row">
 						<div class="col-xs-6 col-sm-6 col-md-6">
 							<div class="form-group">
@@ -56,7 +129,7 @@
 
 	</div>
 
-	<?=!include_once('scripts.php'); ?>	
+	<?=!include_once('scripts.php'); ?>
 
 </body>
 </html>
